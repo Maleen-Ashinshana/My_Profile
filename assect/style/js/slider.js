52b416f6dc6f32d66f2aab807987ae0623c5d6a6
Maleen Ashinshana <maleenashinshana70@gmail.com>
2023-05-22 20:11:19 +0530

    responsive part18

--- assect/style/js/slider.js
+++ assect/style/js/slider.js
@@ -3,13 +3,14 @@ let next=document.getElementById('next');
 let prev=document.getElementById('prev');
 
 let active=3;
+
 function loadShow() {
     let stt=0;
     item[active].style.transform=`none`;
-    /* item[i].style.zIndex = 1;*/
+     /*item[i].style.zIndex = 1;*/
     item[active].style.filter='none';
     item[active].style.opacity= 1;
-    for(var i =active + 1;i < item.length; i++){
+    for(var i =active +1;i < item.length; i++){
         stt++;
         item[i].style.transform=`translateX(${120*stt}px) scale(${1 - 0.15*stt}) perspective(16px) rotateY(-1deg)`;
          /* item[i].style.zIndex = ++stt;*/
@@ -20,7 +21,7 @@ function loadShow() {
     for (var i =active -1;i >= 0; i--){
         stt++;
         item[i].style.transform=`translateX(${-120*stt}px) scale(${1 - 0.15*stt}) perspective(16px) rotateY(1deg)`;
-          /*item[i].style.zIndex = --stt;*/
+         /* item[i].style.zIndex = --stt;*/
         item[i].style.filter='blur(5px)';
         item[i].style.opacity=stt > 2 ? 0 : 0.6;
     }
